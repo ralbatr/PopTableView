13743c5510963b4ae7fcadf9301aad3748d5fe05
ralbatr <ralbatr@jbgsn.local>
2015-06-23 15:27:01 +0800

    1 初始化工程

---
+++ PopTableView/PopTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopTableView
+//
+//  Created by Ralbatr on 15/6/23.
+//  Copyright (c) 2015年 ralbatr Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopTableView/PopTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopTableView
+//
+//  Created by Ralbatr on 15/6/23.
+//  Copyright (c) 2015年 ralbatr Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
